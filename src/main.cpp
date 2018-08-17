c5f1f986363410802fc04c2bea1c95089360622e
Federico Berra <fede.berra@hotmail.it>
2018-08-17 15:49:10 +0200

    First vscode setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "lib/Ising.h"
+using namespace std;
 
 int main(int argc, char const *argv[]) {
   printf("Hello world!");
---
+++ test/test.cpp
@@ -0,0 +1,1 @@
+#include "lib/IsingTest.cpp"
