52f6b725caacb9cb55c3c70f9486131689f5a75b
Federico Berra <fede.berra@hotmail.it>
2018-08-20 18:07:16 +0200

    Fix sa test name

--- test/test.cpp
+++ test/test.cpp
@@ -3,4 +3,4 @@
 #include "models/NodeTest.h"
 #include "models/Arc2Test.h"
 #include "models/Ising2dTest.h"
-#include "algorithms/SimulatedAnnealing.h"
+#include "algorithms/SimulatedAnnealingTest.h"
